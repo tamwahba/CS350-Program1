36e5bd535287313a0af4bf6664ee6f87fabe4fa2
ConnorStutsrim <connorsttsrm@gmail.com>
2016-03-15 13:25:02 -0400

    Add simple process intialization and dependencies
    Add code to intialize the focused process and the field of processes in
    general.
    For the focus process, PID and address size are relatively final. User
    specification for remaining memory accesses is not yet in place, so
    that detail is currently static. The start specification is awaiting
    further clarification. The last page spec is a running spec and will be
    set and changed elsewhere.
    For the process field, PID and address size are relatively final. Rem-
    aining memory accesses is currentl generated as a range between 1000 and
    11000. The start spec is awaiting further clarification. The last page
    spec is a running spec and will be set and changed elsewhere.
    The time and stdlib header were added to support random number
    generation.

--- lab4.c
+++ lab4.c
@@ -1,3 +1,7 @@
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 typedef struct proc {
     int pid;
     int remaining;
@@ -10,16 +14,32 @@ typedef struct proc {
 
 int main(int argc, char* argv[]) {
     // default settings
+    srand(time(NULL));
     int processId = 1;
-    int numProcesses = 1;
+    int numProcesses = 10;
     int mainAddress = 4, mainLocality = 2;
     int rangeAddress = 2, locality = 2;
 
     // read input and check bounds for parameters
     
     process processes[numProcesses];
+    int runningStart = 1;
     // generate processes, put into processes array.
-   
+    processes[0].pid = processId;
+    processes[0].remaining = 1000;
+    processes[0].start = runningStart++;
+    processes[0].addressSize = mainAddress;
+    processes[0].lastPage = 0;
+
+    for(int i = 1; i < numProcesses; i++) {
+        processes[i].pid = i + processId;
+        processes[i].remaining = (rand() % 10000) + 1000;
+        processes[i].start = runningStart++;
+        if(rangeAddress == 1) processes[i].addressSize = (rand() % 5) + 1;
+        else if(rangeAddress == 2) processes[i].addressSize = (rand() % 15) + 5;
+        else processes[i].addressSize = (rand() % 30) + 20;
+        processes[i].lastPage = 0;
+    }
 
     // loop over each process checking if it should start now. If yes, print starting otherwise
     // randomly select processes for memory access, decrementing remaining
